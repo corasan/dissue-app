fbd9286b10d346a5d644ba251a6f5f55b9e7427a
Henry Paulino <henrypl360@gmail.com>
2019-12-03 14:44:52 -0500

    open auth modal

--- app/main.dev.js
+++ app/main.dev.js
@@ -10,42 +10,42 @@
  *
  * @flow
  */
-import { app, BrowserWindow } from 'electron';
-import { autoUpdater } from 'electron-updater';
-import log from 'electron-log';
-import MenuBuilder from './menu';
+import { app, BrowserWindow, ipcMain, session } from 'electron'
+import { autoUpdater } from 'electron-updater'
+import log from 'electron-log'
+import MenuBuilder from './menu'
 
 export default class AppUpdater {
   constructor() {
-    log.transports.file.level = 'info';
-    autoUpdater.logger = log;
-    autoUpdater.checkForUpdatesAndNotify();
+    log.transports.file.level = 'info'
+    autoUpdater.logger = log
+    autoUpdater.checkForUpdatesAndNotify()
   }
 }
 
-let mainWindow = null;
+let mainWindow = null
 
 if (process.env.NODE_ENV === 'production') {
-  const sourceMapSupport = require('source-map-support');
-  sourceMapSupport.install();
+  const sourceMapSupport = require('source-map-support')
+  sourceMapSupport.install()
 }
 
 if (
   process.env.NODE_ENV === 'development' ||
   process.env.DEBUG_PROD === 'true'
 ) {
-  require('electron-debug')();
+  require('electron-debug')()
 }
 
 const installExtensions = async () => {
-  const installer = require('electron-devtools-installer');
-  const forceDownload = !!process.env.UPGRADE_EXTENSIONS;
-  const extensions = ['REACT_DEVELOPER_TOOLS', 'REDUX_DEVTOOLS'];
+  const installer = require('electron-devtools-installer')
+  const forceDownload = !!process.env.UPGRADE_EXTENSIONS
+  const extensions = ['REACT_DEVELOPER_TOOLS', 'REDUX_DEVTOOLS']
 
   return Promise.all(
     extensions.map(name => installer.default(installer[name], forceDownload))
-  ).catch(console.log);
-};
+  ).catch(console.log)
+}
 
 /**
  * Add event listeners...
@@ -55,16 +55,16 @@ app.on('window-all-closed', () => {
   // Respect the OSX convention of having the application in memory even
   // after all windows have been closed
   if (process.platform !== 'darwin') {
-    app.quit();
+    app.quit()
   }
-});
+})
 
 app.on('ready', async () => {
   if (
     process.env.NODE_ENV === 'development' ||
     process.env.DEBUG_PROD === 'true'
   ) {
-    await installExtensions();
+    await installExtensions()
   }
 
   mainWindow = new BrowserWindow({
@@ -74,32 +74,46 @@ app.on('ready', async () => {
     webPreferences: {
       nodeIntegration: true
     }
-  });
+  })
 
-  mainWindow.loadURL(`file://${__dirname}/app.html`);
+  mainWindow.loadURL(`file://${__dirname}/app.html`)
 
   // @TODO: Use 'ready-to-show' event
   //        https://github.com/electron/electron/blob/master/docs/api/browser-window.md#using-ready-to-show-event
   mainWindow.webContents.on('did-finish-load', () => {
     if (!mainWindow) {
-      throw new Error('"mainWindow" is not defined');
+      throw new Error('"mainWindow" is not defined')
     }
     if (process.env.START_MINIMIZED) {
-      mainWindow.minimize();
+      mainWindow.minimize()
     } else {
-      mainWindow.show();
-      mainWindow.focus();
+      mainWindow.show()
+      mainWindow.focus()
     }
-  });
+  })
+
+  const clientId = 'Iv1.aa2791eb59403f7d'
+  const url = `https://github.com/login/oauth/authorize?scope=user:email&client_id=${clientId}`
+  const authModal = new BrowserWindow({
+    parent: mainWindow,
+    modal: true,
+    show: false
+  })
+  authModal.loadURL(url)
+
+  ipcMain.on('open-auth', () => {
+    authModal.show()
+  })
 
   mainWindow.on('closed', () => {
-    mainWindow = null;
-  });
+    session.defaultSession.clearCache()
+    mainWindow = null
+  })
 
-  const menuBuilder = new MenuBuilder(mainWindow);
-  menuBuilder.buildMenu();
+  const menuBuilder = new MenuBuilder(mainWindow)
+  menuBuilder.buildMenu()
 
   // Remove this if your app does not use auto updates
   // eslint-disable-next-line
   new AppUpdater();
-});
+})
