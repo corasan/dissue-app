9682a4ccbe65698841aadda52a66069d2aec695c
Henry Paulino <henrypl360@gmail.com>
2019-12-05 00:33:25 -0500

    remove semi colons and minor configurations

--- app/Routes.js
+++ app/Routes.js
@@ -1,9 +1,9 @@
-import React from 'react';
-import { Switch, Route } from 'react-router';
-import routes from './constants/routes';
-import App from './containers/App';
-import HomePage from './containers/HomePage';
-import CounterPage from './containers/CounterPage';
+import React from 'react'
+import { Switch, Route } from 'react-router'
+import routes from './constants/routes'
+import App from './containers/App'
+import HomePage from './containers/HomePage'
+import CounterPage from './containers/CounterPage'
 
 export default () => (
   <App>
@@ -12,4 +12,4 @@ export default () => (
       <Route path={routes.HOME} component={HomePage} />
     </Switch>
   </App>
-);
+)
